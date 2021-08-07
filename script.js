0161e297972b3edb3dd1354eff342ec5dcabd225
Jihye Pak <jiji.pak41@gmail.com>
2021-08-08 00:30:44 +0100

    change add list function to prevent adding empty string to the list

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 'use strict';
 
+// button click event
+
 const btn = document.querySelector('.list__bottom > form > button');
 const text = document.querySelector('#text');
 const ul = document.querySelector('#list__body--container > ul');
 
 let new_item = '';
 
+// function - when click button, add items to the list
 function addList(e) {
   e.preventDefault();
 
   const template = document.querySelector('#list__template');
   const domFrag = template.content.cloneNode(true);
 
-  domFrag.querySelector('.item__name').textContent = new_item.toUpperCase();
-  domFrag.querySelector('.item__bin').innerHTML =
-    '<i class="far fa-trash-alt"></i>';
-  ul.append(domFrag);
-  text.value = 'Type..';
+  if (new_item.length > 0) {
+    domFrag.querySelector('.item__name').textContent = new_item.toUpperCase();
+    domFrag.querySelector('.item__bin').innerHTML =
+      '<i class="far fa-trash-alt"></i>';
+    ul.append(domFrag);
+    text.value = 'Type..';
+  }
+
+  new_item = '';
   text.focus();
 }
 
+// function - when click input section, empty input value
 function emptyInput() {
   text.value = '';
 }
 
+//function - when input has changed update input value
 function updateText() {
   new_item = text.value;
 }
